b147fc7b9442203bc96a5317737167f902e5615e
Sasank Krishna Talasila <sasanktk@gmail.com>
2013-12-16 17:35:43 -0500

    Delete files

--- sql/delete_user.sql
+++
@@ -1,4 +0,0 @@
---Utility file to delete a user
-delete from authentication where login_id='xyz@gmail.com';
-delete from user_accounts where login_user_id='xyz@gmail.com';
-delete from user_data where login_user_id='xyz@gmail.com';
