675cf2defa0d8cffeb8f9bfe2e1653a2df31aae3
Krishna Sasank  Talasila <sasanktk@gmail.com>
2013-12-16 19:25:08 -0500

    Update mytab.sql

--- sql/mytab.sql
+++ sql/mytab.sql
@@ -1,11 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 09, 2013 at 10:47 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.16
+-- MyTab installation script
+-- Needs MySQL version > 5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
